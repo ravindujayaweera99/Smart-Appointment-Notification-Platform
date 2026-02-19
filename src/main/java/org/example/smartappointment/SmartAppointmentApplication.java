09b81c027c6c799160ea33c3675cc3a202cbd71e
Ravindu Jayaweera <ravindujayaweera99@Ravindus-MacBook-Air.local>
2026-02-19 19:44:37 +0530

    project initialized

---
+++ src/main/java/org/example/smartappointment/SmartAppointmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAppointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAppointmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/smartappointment/SmartAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
